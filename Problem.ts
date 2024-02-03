b4ef07e6ef8ab27f697b979f4cf590977931bf72
Daniele Pizzi <pizzi.daniele1993@gmail.com>
2024-02-03 13:34:21 +0100

    Create Problem.ts

---
+++ Problem.ts
@@ -0,0 +1,13 @@
+function smallestNegativeBalance(debts: string[][]): string[] {
+    return[]
+}
+
+var example = [
+    ["Alex","Blake","2"],
+    ["Blake","Alex","2"],
+    ["Casey","Alex","5"],
+    ["Blake","Alex","4"],
+    ["Alex","Blake","7"],
+    ["Alex","Casey","1"],
+]
+smallestNegativeBalance(example)
